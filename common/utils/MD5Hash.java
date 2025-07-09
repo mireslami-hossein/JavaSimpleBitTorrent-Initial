84c04828f68dc41cded630c8e8e51624d9c167ff
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-07-09 23:00:21 +0330

    MD5 hash completed

--- common/utils/MD5Hash.java
+++ common/utils/MD5Hash.java
@@ -1,13 +1,36 @@
 package common.utils;
 
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+
 public class MD5Hash {
 	public static String HashFile(String filePath) {
-		// TODO: Calculate MD5 hash of file
 		// 1. Open file input stream
-		// 2. Create message digest
-		// 3. Read file in chunks and update digest
-		// 4. Convert digest to hex string
+		try (FileInputStream fileIn = new FileInputStream(filePath)) {
+			// 2. Create message digest
+			MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+
+			// 3. Read file in chunks and update digest
+			byte[] buffer = new byte[1024];
+			int bytesRead;
+			while ((bytesRead = fileIn.read(buffer)) != -1) {
+				messageDigest.update(buffer, 0, bytesRead);
+			}
+
+			// 4. Convert digest to hex string
+			byte[] digest = messageDigest.digest();
+			StringBuilder fileHash = new StringBuilder();
+			for (byte b : digest) {
+				String hex = Integer.toHexString(0xff & b);
+				if (hex.length() == 1) fileHash.append('0');
+				fileHash.append(hex);
+			}
+
+			return fileHash.toString();
 		// 5. Handle errors
-		throw new UnsupportedOperationException("MD5 hash calculation not implemented yet");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
