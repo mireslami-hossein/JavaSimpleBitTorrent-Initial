ec8119a43553eaf0c54b2d5d845b863e39fa3967
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-07-09 23:20:38 +0330

    FileUtils completed

--- common/utils/FileUtils.java
+++ common/utils/FileUtils.java
@@ -1,23 +1,41 @@
 package common.utils;
 
-import java.util.Map;
+import java.io.File;
+import java.util.*;
 
 public class FileUtils {
 
 	public static Map<String, String> listFilesInFolder(String folderPath) {
 		// TODO: List files in folder
-		// 1. Create folder object
-		// 2. Get list of files
 		// 3. Calculate MD5 hash for each file
 		// 4. Return map of filename to hash
-		throw new UnsupportedOperationException("List files in folder not implemented yet");
+		// 1. Create folder object
+		Map<String, String> fileHashes = new HashMap<>();
+		File file = new File(folderPath);
+		if (file.exists() && file.isDirectory()) {
+			// 2. Get list of files
+			File[] files = file.listFiles();
+			for (File f : files) {
+				if (f.isFile())
+					fileHashes.put(f.getName(), MD5Hash.HashFile(f.getPath()));
+			}
+		}
+		return fileHashes;
 	}
 
 	public static String getSortedFileList(Map<String, String> files) {
-		// TODO: Get sorted file list
 		// 1. Check if files map is empty
+		if (files.isEmpty()) return "Repository is empty.";
+
 		// 2. Sort file names
+		Map<String, String> sortedFiles = new TreeMap<>(files);
+
 		// 3. Create formatted string with names and hashes
-		throw new UnsupportedOperationException("Get sorted file list not implemented yet");
+		StringBuilder sortedFileList = new StringBuilder();
+		for (String key : sortedFiles.keySet()) {
+			sortedFileList.append(key + " " + sortedFiles.get(key) + "\n");
+		}
+		return sortedFileList.toString();
 	}
+
 }
